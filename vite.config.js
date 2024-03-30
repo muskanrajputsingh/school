c40a85ee1afe2f32969ad72ec6e2fdd7801203a7
muskanrajputsingh <muskansingh7105@gmail.com>
2024-03-30 13:03:08 +0530

    updated

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   server:{
     proxy:{
-      "/register":"http://localhost:3000/",
-      "/regdashboard":"http://localhost:3000/",
-      "/logindashboard":"http://localhost:3000/"
+      // "/register":"http://localhost:3000/",
+      // "/regdashboard":"http://localhost:3000/",
+      // "/logindashboard":"http://localhost:3000/"
+      "/register":"https://schoolnode-2uw8.onrender.com/",
+      "/regdashboard":"https://schoolnode-2uw8.onrender.com/",
+      "/logindashboard":"https://schoolnode-2uw8.onrender.com/"
     },
   },
   plugins: [react()],
